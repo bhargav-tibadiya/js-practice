d1d121cbadd998210ee02e87a8bdcd64d7be6d4e
bhargav-tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-10-29 12:59:46 +0530

    Added Variables and Functions

---
+++ 1. Basics/index.js
@@ -0,0 +1,28 @@
+// Variables 
+
+console.log('--> Variables <--')
+
+var num1 = 10;
+var num2 = 20;
+
+// console.log('num1 :', num1)
+// console.log('num2 :', num2)
+
+{
+  let num3 = 30;
+  // console.log('num3 :', num3)
+}
+
+
+// Functions
+
+console.log('--> Functions <--')
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+console.log('addNumbers(5,10) :', addNumbers(5, 10))
+
+
+// Array
